d76b6f76fa561f822b2c21e003ea8351e24cb218
IvanSavDev <ivan-savenko@mail.ru>
2022-08-06 13:21:12 +0300

    feat: remove try catch

--- async-race/async-race-app/src/api.ts
+++ async-race/async-race-app/src/api.ts
@@ -1,4 +1,4 @@
-import { ICar, ICars, ICarsWinners } from './dataInterfaces';
+import { ICar, ICars, ICarsWinners } from './types/dataInterfaces';
 
 const apiBasePath = 'http://localhost:3000';
 
@@ -6,21 +6,14 @@ const garagePath = `${apiBasePath}/garage`;
 const winnersPath = `${apiBasePath}/winners`;
 const enginePath = `${apiBasePath}/engine`;
 
-export const getCars = async (
-  page = 1,
-  limit = 7
-): Promise<ICars | undefined> => {
-  try {
-    const response = await fetch(`${garagePath}?_page=${page}&_limit=${limit}`);
-    const countCars = response.headers.get('X-Total-Count');
-    const dataCars = await response.json();
-    return {
-      cars: dataCars,
-      count: Number(countCars),
-    };
-  } catch (e) {
-    console.log(e);
-  }
+export const getCars = async (page = 1, limit = 7): Promise<ICars> => {
+  const response = await fetch(`${garagePath}?_page=${page}&_limit=${limit}`);
+  const countCars = response.headers.get('X-Total-Count');
+  const dataCars = await response.json();
+  return {
+    cars: dataCars,
+    count: Number(countCars),
+  };
 };
 
 export const getCar = async (id: number): Promise<ICar> => {
@@ -29,13 +22,16 @@ export const getCar = async (id: number): Promise<ICar> => {
   return dataCar;
 };
 
-export const createCar = async (name: string, color: string) => {
+export const createCar = async (insertDataCar: {
+  name: string;
+  color: string;
+}): Promise<ICar> => {
   const response = await fetch(`${garagePath}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ name, color }),
+    body: JSON.stringify(insertDataCar),
   });
   const dataCar = await response.json();
   return dataCar;
@@ -61,7 +57,9 @@ export const updateCar = async (id: number, name: string, color: string) => {
   return dataCar;
 };
 
-export const startCar = async (id: number) => {
+export const startCar = async (
+  id: number
+): Promise<{ velocity: number; distance: number }> => {
   const response = await fetch(`${enginePath}?id=${id}&status=started`, {
     method: 'PATCH',
   });
@@ -81,8 +79,9 @@ export const driveCar = async (id: number) => {
   const response = await fetch(`${enginePath}?id=${id}&status=drive`, {
     method: 'PATCH',
   });
-  const dataCar = await response.json();
-  return dataCar;
+  return response.status;
+  // const dataCar = await response.json();
+  // return dataCar;
 };
 
 export const getWinner = async (id: number) => {
@@ -96,20 +95,16 @@ export const getWinners = async (
   sort = 'id',
   order = 'ASC',
   limit = 10
-): Promise<ICarsWinners | undefined> => {
-  try {
-    const response = await fetch(
-      `${winnersPath}?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`
-    );
-    const countCars = response.headers.get('X-Total-Count');
-    const dataCars = await response.json();
-    return {
-      winners: dataCars,
-      count: Number(countCars),
-    };
-  } catch (error) {
-    console.log(error);
-  }
+): Promise<ICarsWinners> => {
+  const response = await fetch(
+    `${winnersPath}?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`
+  );
+  const countCars = response.headers.get('X-Total-Count');
+  const dataCars = await response.json();
+  return {
+    winners: dataCars,
+    count: Number(countCars),
+  };
 };
 
 export const createWinner = async (id: number, wins: number, time: number) => {

bb3a0f37622c863cc1903ccea17457767700d1a4
IvanSavDev <ivan-savenko@mail.ru>
2022-08-08 17:42:22 +0300

    feat: add enum

---
+++ async-race/async-race-app/src/enum/enum.ts
@@ -0,0 +1,20 @@
+export enum SortTypes {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+export enum SortCategory {
+  wins = 'wins',
+  time = 'time',
+}
+
+export enum Pages {
+  garage = 'garage',
+  winners = 'winners',
+}
+
+export enum RaceStatus {
+  start = 'start',
+  process = 'process',
+  finished = 'finished',
+}

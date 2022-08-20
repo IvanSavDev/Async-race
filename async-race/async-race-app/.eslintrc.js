960da4f87dad9c4ee4dcb5112a995fabeddfc800
IvanSavDev <ivan-savenko@mail.ru>
2022-08-20 15:49:45 +0300

    refactor: change file with crlf to lf

--- async-race/async-race-app/.eslintrc.js
+++ async-race/async-race-app/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
   ignorePatterns: ['node_modules', 'webpack.config.js'],
   plugins: ['@typescript-eslint'],
